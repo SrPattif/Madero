7653a60f74195cd108a71692ef2e0186b581e73f
SrPattif <omininuguxx@gmail.com>
2023-07-10 08:51:34 -0300

    before database changes

--- libs/databaseConnection.php
+++ libs/databaseConnection.php
@@ -6,7 +6,7 @@
     $dotenv = Dotenv\Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT']);
     $dotenv->load();
 
-    $mysqli = new mysqli($_ENV['DATABASE_HOST'], $_ENV['DATABASE_USERNAME'], $_ENV['DATABASE_PASSWORD'], $_ENV['DATABASE_NAME']);
+    $mysqli = new mysqli($_ENV['DATABASE_HOST'], $_ENV['DATABASE_USERNAME'], $_ENV['DATABASE_PASSWORD']);
     if($mysqli->connect_errno) {
         echo('Ocorreu um erro ao tentar se conectar aos serviços.');
     }
