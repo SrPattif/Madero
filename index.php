b43008ce865479cdabcf2e067b3f39b1c077b60a
SrPattif <omininuguxx@gmail.com>
2023-09-03 18:01:55 -0300

    padronização de titulo da pagina

--- index.php
+++ index.php
@@ -62,7 +62,7 @@ foreach($rowsModulos as $modulo) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Controladoria Grupo Madero</title>
+    <title>Controladoria | Grupo Madero</title>
 
     <!-- Estilos -->
     <link rel="stylesheet" href="./defaultStyle.css" />
