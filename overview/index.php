ee209d61786aeed8e376a818636ff67d432f1b60
SrPattif <omininuguxx@gmail.com>
2023-09-03 22:17:55 -0300

    página de visão geral do projeto.

---
+++ overview/index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-2F2Z7S0VR0"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    </script>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>O Novo Sistema de Controladoria do Grupo Madero</title>
+
+    <!-- Estilos -->
+    <link rel="stylesheet" href="./index.css" />
+
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body>
+    <main>
+        <div class="page-content">
+            <div class="landing">
+                <div class="title-hero">
+                    <div class="title">
+                        <img class="madero-logo" src="/assets/images/madero-white.png" alt="">
+                        <div class="tt-tit">A Otimização da Controladoria</div>
+                        <div class="tt-desc">Proposta de implementação do sistema para aumentar a fluidez e produtividade.</div>
+                    </div>
+                </div>
+            </div>
+            <div class="data-list">
+                <div class="data">
+                    <div class="data-header">
+                        Quase 30 boletos
+                    </div>
+                    <div class="data-description">
+                        Esse é o número de boletos que foram lançados <span class="highlight">com
+                            inconsistências</span>.
+                        <br>
+                        Esses erros <span class="highlight">impossibilitam o pagamento do boleto</span> e, algumas
+                        vezes, geram o pagamento de <span class="highlight">juros por atraso</span>.
+                    </div>
+                    <div class="data-source">
+                        Fonte: Análise da quantidade de boletos de aluguel e ocupação comunicados pelo setor financeiro
+                        que não foram encontrados ou estão com valores divergentes.
+                    </div>
+                </div>
+
+                <div class="data" style="text-align: right;">
+                    <div class="data-header">
+                        Validação dos Dados Informados
+                    </div>
+                    <div class="data-description">
+                        A maior parte das validações, como <b>retenção de imposto de renda</b>, são <span
+                            class="highlight">realizadas automaticamente</span>.
+                        <br>
+                        O título <span class="highlight">só poderá ser gerado</span> se estiver de acordo com os
+                        <b>dados fornecidos pelo boleto</b>.
+                    </div>
+                </div>
+
+                <div class="data">
+                    <div class="data-header">
+                        TAP - Título Automático Posterior
+                    </div>
+                    <div class="data-description">
+                        Nesse método, o título não é gerado <span class="highlight">no momento da medição</span>, mas
+                        sim <b>posteriormente</b>.
+                        <br>
+                        Isso evita que o operador tenha que <span class="highlight">esperar até a geração do
+                            título</span> para <b>renomear o arquivo</b> e <b>copiar até a pasta</b> do Financeiro.
+                        <br>
+                        Tudo isso é feito <b>automaticamente</b>.
+                    </div>
+                </div>
+
+                <div class="data" style="text-align: right;">
+                    <div class="data-header">
+                        Melhor Análise de Taxas
+                    </div>
+                    <div class="data-description">
+                        A medição poderá ser realizada em <span class="highlight">inúmeros produtos</span> no sistema,
+                        mas <b>lançados de maneira unificada</b> no Protheus.
+                        <br>
+                        Isso nos permite <b>entender profundamente</b> os valores pagos, permitindo traçar estratégias
+                        <span class="highlight">para melhorar os números</span>.
+                    </div>
+                </div>
+
+                <div class="data">
+                    <div class="data-header">
+                        Elementos de Maior Eficiência
+                    </div>
+                    <div class="data-description">
+                        Hoje, uma medição para um <span class="highlight">boleto de ocupação</span> leva, em média, de <b>50 a 140 segundos</b>.
+                        <br>
+                        Aqui, dados mais específicos, como <b>TES</b>, <b>número de contrato</b> e <b>centro de custo</b>, são informados automaticamente.
+                        <br>
+                        Isso, junto com os <b>atalhos de teclado</b> e <b>fluidez do site</b>, pode fazer com que o tempo de lançamento seja <span class="highlight"> até 60% menor</span>.
+                    </div>
+                </div>
+
+                <div class="data" style="text-align: right;">
+                    <div class="data-header">
+                        Aplicável em Múltiplos Temas
+                    </div>
+                    <div class="data-description">
+                        O sistema pode atender a <span class="highlight">diversas divisões da controladoria</span>, em todos os assuntos possíveis.
+                        <br>
+                        <b>Análise e rateio de despesas</b>, <b>gerenciamento de síndicos</b>, <b>informes de venda</b> e <b>lançamento de boletos</b> são apenas exemplos.
+                    </div>
+                </div>
+
+                <div class="footer">
+                    <div class="contact">
+                        <b>Gustavo Rodrigues</b>
+                        <br>gustavo.rodrigues@payoo.com.br
+                        <br>(41) 9 9735-2115
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
