aa919b60ca67a33920f75421d3066e6f9138c194
SrPattif <omininuguxx@gmail.com>
2023-09-30 12:32:35 -0300

    problema com data de baixa vazia aparecendo como dia atual resolvido.

--- moradias/reembolsos/alojamentos/detalhes/index.php
+++ moradias/reembolsos/alojamentos/detalhes/index.php
@@ -133,10 +133,19 @@
                     <?php
                             } else {
                                 $amount = "R$ " . number_format($houseData['valor_total'], 2, ",", ".");
-                                $expiresDateObj = date_create($houseData['data_vencimento']);
-                                $expiresDate = date_format($expiresDateObj, "d/m/Y");
-                                $paidDateObj = date_create($houseData['data_baixa']);
-                                $datePaid = date_format($paidDateObj, "d/m/Y");
+                               
+                                $datePaid = "-";
+                                if(!empty($houseData['data_baixa'])) {
+                                    $paidDateObj = date_create($houseData['data_baixa']);
+                                    $datePaid = date_format($paidDateObj, "d/m/Y");
+                                }
+
+                                $expiresDate = "-";
+                                if(!empty($houseData['data_vencimento'])) {
+                                    $expiresDateObj = date_create($houseData['data_vencimento']);
+                                    $expiresDate = date_format($expiresDateObj, "d/m/Y");
+                                }
+
 
                                 $daysDifference = $paidDateObj->diff($expiresDateObj)->format("%a");
                                 $differenceStatus = "";
